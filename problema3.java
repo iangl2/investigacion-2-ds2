5d44235b27c9feaf62c4ec4ce72059ffd4128bd1
Ian Lee <ianlee0203@gmail.com>
2024-09-06 18:07:25 -0500

    Third Commit here I stablish which part of the problem  Raul my teammate is going to do

--- problema3.java
+++ problema3.java
@@ -2,7 +2,12 @@
  * problema3
  */
 public class problema3 {
-    
+    public static void punto1() {
+        //aquí trabajo yo 
+    }
+    public static void punto2() {
+        //aqui trabaja Raul
+    }
     public static void main(String[] args) {
         
     }
