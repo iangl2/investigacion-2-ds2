d68eeabcbcd3a5fe070088e00b1cf795b1b4fb48
Ian Lee <ianlee0203@gmail.com>
2024-09-06 19:03:35 -0500

    Fourth Commit, Enhanced the menu

--- problema3.java
+++ problema3.java
@@ -1,14 +1,31 @@
 /**
  * problema3
  */
+import javax.swing.JOptionPane;
+
 public class problema3 {
     public static void punto1() {
         //aquí trabajo yo 
+        System.out.println("hello world this the first method");
     }
     public static void punto2() {
         //aqui trabaja Raul
+        System.out.println("hello world this the second method");
+
     }
     public static void main(String[] args) {
-        
+        String [] options={"Problema 1", "Problema 2", "Salir"};
+        while (true) {
+            var selection= JOptionPane.showOptionDialog(null, "Discount Percentage: ", 
+            "Select a Discount Option", 0, JOptionPane.INFORMATION_MESSAGE, null, options, options[0]);
+
+            switch (selection) {
+                case 0 -> punto1();
+                case 1 -> punto2();
+                default -> {
+                    return;
+                }
+            }
+        }
     }
 }
