bbbc40696806e3a082dc0aa80647e5bca12f9b73
Raul <raul1911.g@gmail.com>
2024-09-11 16:03:59 -0500

    Commit_1, solo falta distribucion

--- problema3.java
+++ problema3.java
@@ -10,7 +10,40 @@ public class problema3 {
     }
     public static void punto2() {
         //aqui trabaja Raul
-        // branch test
+        int ind=0, cont=0, reset;
+        float notas[] =new float[5], promedio=0, n_mayor=0, n_menor=0;
+        String[] opciones  ={"Siguiente", "Cancelar"};
+        do {
+            reset=1;            
+               var opcion = JOptionPane.showOptionDialog(null, "Bienvenido al libro de calificaciones para \n Introducción a la Programación", "Libro de Calificaciones", 0, 3, null, opciones, opciones[0]);
+                if (opcion == 0) {
+                    try {                        
+                        for (ind = 0; ind < 5; ind++) {
+                            cont=ind+1;
+                            String nota = JOptionPane.showInputDialog("Entre la nota del estudiante "+cont+" (1.0-5.0)");
+                            notas[ind] = Float.parseFloat(nota);  
+                            if(notas[ind]>=1 && notas[ind]<=5)
+                                promedio = promedio + notas[ind];
+                            else{
+                                throw new Exception();
+                            }
+                            if(notas[ind]<n_menor)
+                                n_menor=notas[ind];
+                            else{
+                                if(notas[ind]>n_mayor)
+                                    n_mayor=notas[ind];
+                            }
+                        }
+                        promedio=(float) (promedio/5.0);
+                        JOptionPane.showMessageDialog(null, "Bienvenido al libro de calificaciones para \nIntroducción a la Programación\n\nLas Calificaciones son: \n\nEstudiante 1:"+notas[0]+"\nEstudiante 2: " +notas[1]
+                        +"\nEstudiante 3: "+notas[2]+"\nEstudiante 4: "+notas[3]+"\nEstudiante 5: "+notas[4]+ String.format("\n\nEl resultado es: %.1f",promedio)+"\nLa nota más baja es: "+n_menor+"\nLa nota más alta es: "+n_mayor+"\n\nDistribución de las notas: \n");                    
+                    } catch (Exception e) {
+                        reset = JOptionPane.showConfirmDialog(null, "Uno de los datos entrados no es válido o no está dentro del rango. ¿Intentar de nuevo?", "Error", JOptionPane.YES_NO_OPTION, JOptionPane.ERROR_MESSAGE);
+                    } 
+                                        
+                }            
+        } while (reset==0);
+        
         System.out.println("hello world this the second method");
 
     }
